f2ed726846ea95a0f885a2dbdea5723430979f40
имя <lalandrynka22@gmail.com>
2026-09-17 20:27:08 +0200

    Initial commit

---
+++ main-panel/main.js
@@ -0,0 +1,8 @@
+//connection to login panel
+function log_page() {
+    window.location.href = "/login-panel/log.html";
+}
+//connection to  registration panel
+function reg_page() {
+    window.location.href = "/registration-panel/reg.html";
+}
---
+++ registration-panel/reg.js
@@ -0,0 +1,4 @@
+//connection to  main panel
+function main_page() {
+    window.location.href = "/main-panel/main.html";
+}
